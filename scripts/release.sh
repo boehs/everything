8130d412ced030cf155ba51788637c0f319266ce
uncenter <47499684+uncenter@users.noreply.github.com>
2023-12-29 21:53:35 -0500

    chore: remove comments

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,24 +1,17 @@
 #!/usr/bin/env sh
 
-# Navigate to the 'lib' directory or exit if the directory doesn't exist.
 cd ./lib/ || exit
 
-# For each sub-directory of 'lib' represented by 'dir'
 for dir in */; do
-    # Navigate into 'dir' or continue to the next 'dir' if this fails
-    cd "$dir" || continue
+    cd "$dir"
 
-    # Print the name of the directory we're attempting to publish
-    echo "Publishing directory $dir"
+    echo "Publishing directory $dir..."
 
-    # Try to publish the npm package publicly. 
-    # If successful, print a success message; if not, print a failure message.
     if npm publish --access public; then
-        echo "Successfully published $dir"
+        echo "Successfully published $dir!"
     else
-        echo "Failed to publish $dir"
+        echo "Oh no! Failed to publish $dir!"
     fi
 
-    # Navigate back to the parent ('lib') directory
     cd ..
 done
